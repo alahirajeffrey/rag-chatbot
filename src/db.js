dbdc917f1918be10940e88274801ae5be6573786
alahirajeffrey <alahirajeffrey@gmail.com>
2026-07-17 15:13:32 +0100

    database config

---
+++ src/db.js
@@ -0,0 +1,25 @@
+const { Pool } = require("pg");
+
+// init pg pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  onConnect: () => console.log("Connected to pg vector"),
+});
+
+// init database
+const initializeDatabase = async () => {
+  await pool.query(`CREATE EXTENSION IF NOT EXISTS vector`);
+
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS documents (
+      id UUID PRIMARY KEY,
+      content TEXT NOT NULL,
+      source TEXT NOT NULL,
+      embedding VECTOR(3072)
+    )
+  `);
+
+  console.log("Database migration run successfully");
+};
+
+module.exports = { pool, initializeDatabase };
